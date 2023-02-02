fb218032438a4b825198b5b259a5f35cd3937edb
Rahul singh <rahulsingh.83748@gmail.com>
2023-02-02 16:32:42 +0530

    notification

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -13,72 +13,86 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  late FlutterTts flutterTts;
+
+  String token = "";
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    flutterTts = FlutterTts();
-
     /// 1. This method call when app in terminated state and you get a notification
     /// when you click on notification app open from terminated state and you can get notification data in this method
-
     FirebaseMessaging.instance.getInitialMessage().then((message) {
-        if (message != null) {
-         print("-------------New Notification-----------------");
-         
-        }
-      },
-    );
+      if (message != null) {
+        print("-------------New Notification-----------------");
+        _speak("new notification from getInitialMessage");
+      }
+    },);
 
     /// 2. This method only call when App in foreground it mean app must be opened
     FirebaseMessaging.onMessage.listen((message) {
-        print("on listen");
-        if (message.notification != null) {
-          print(message.notification!.title);
-          print(message.notification!.body);
-          _speak(message.notification!.body??"");
-          print("message.data11 ${message.data}");
-          LocalNotificationService.createanddisplaynotification(message);
-
-        }
-      },
+      print("on listen");
+      if (message.notification != null) {
+        print(message.notification!.title);
+        print(message.notification!.body);
+        _speak(message.notification!.body ?? "");
+        print("message.data11 ${message.data}");
+        LocalNotificationService.createanddisplaynotification(message);
+      }
+    },
     );
-
     /// 3. This method only call when App in background and not terminated(not closed)
-    FirebaseMessaging.onMessageOpenedApp.listen((message) {print(
-            "-----------------------message opened from background---------------");
-        if (message.notification != null) {
-          print(message.notification!.title);
-          print(message.notification!.body);
-          print("match id:  ${message.data['_id']}");
-          _speak(message.notification!.body??"");
-
-        }
-      },
+    FirebaseMessaging.onMessageOpenedApp.listen((message) {
+      print("-----------------------message opened from background---------------");
+      if (message.notification != null) {
+        print(message.notification!.title);
+        print(message.notification!.body);
+        // _speak(message.notification!.body ?? "");
+      }
+    },
     );
+    FirebaseMessaging.onBackgroundMessage(backgroundMessageHandler);
+    gettoken();
 
   }
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       body: Center(
         child: Column(
           mainAxisSize: MainAxisSize.min,
-          children:  [
+          children: [
+            SelectableText(token),
+            SizedBox(height: 100,),
             InkWell(onTap: () {
-              _speak("ok");
+              _speak("on tap");
             }, child: const Text("Notification"))
           ],
         ),
       ),
     );
   }
-  Future _speak(String text) async{
-    // var result = await flutterTts.speak("You received 5 rupees Sachin");
-    flutterTts.speak(text).then((value) {
-      print(value.toString());
+
+  gettoken() async {
+    await FirebaseMessaging.instance.getToken().then((value) {
+      setState(() {
+        token = value??"no";
+      });
     });
   }
 
+}
+
+Future<void> backgroundMessageHandler(RemoteMessage message) async {
+  print(message.data);
+  _speak("form background Message Handler");
+}
+
+
+Future _speak(String text) async {
+  // var result = await flutterTts.speak("You received 5 rupees Sachin");
+  FlutterTts flutterTts = FlutterTts();
+  flutterTts.speak(text).then((value) {
+    print(value.toString());
+  });
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,51 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 
 import 'HomePage.dart';
 import 'firebase_options.dart';
 
+late FlutterTts flutterTts;
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   /// initialize firebase  services in app
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-    print('Got a message whilst in the foreground!');
-    print('Message data: ${message.data}');
+  flutterTts = FlutterTts();
 
-    final notification = message.notification;
-    if (notification != null) {
-      print('Message also contained a notification: ${notification.title.toString()}');
-    }
-  });
   final fcmToken = await FirebaseMessaging.instance.getToken();
   print('token: ${fcmToken} token end');
   print('FCM TOKEN: ${fcmToken}');
   await FirebaseMessaging.instance.setAutoInitEnabled(true); //TODO
-
   await FirebaseMessaging.instance.subscribeToTopic("ssixer");
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print('Got a message whilst in the foreground!');
+    print('Message data: ${message.data}');
+    final notification = message.notification;
+    _speak("new new");
+    if (notification != null) {
+      print(
+          'Message also contained a notification: ${notification.title.toString()}');
+    }
+  });
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+
   runApp(const MyApp());
+}
+
+Future _speak(String text) async {
+  // var result = await flutterTts.speak("You received 5 rupees Sachin");
+  flutterTts.speak(text).then((value) {
+    print(value.toString());
+  });
 }
 
 class MyApp extends StatelessWidget {
@@ -38,11 +57,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
       home: const HomePage(),
     );
   }
 }
-

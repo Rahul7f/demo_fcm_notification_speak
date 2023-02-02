70a88851ea132f151b5cec0f5c21aa0aad097bf1
Rahul singh <rahulsingh.83748@gmail.com>
2023-02-02 12:51:01 +0530

    text to speech

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -9,16 +10,34 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late FlutterTts flutterTts;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    flutterTts = FlutterTts();
+
+  }
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
       body: Center(
         child: Column(
-          children: const [
-            Text("Notification")
+          mainAxisSize: MainAxisSize.min,
+          children:  [
+            InkWell(onTap: () {
+              _speak();
+            }, child: Text("Notification"))
           ],
         ),
       ),
     );
   }
+  Future _speak() async{
+    // var result = await flutterTts.speak("You received 5 rupees Sachin");
+    flutterTts.speak("").then((value) {
+      print(value.toString());
+    });
+  }
+
 }
